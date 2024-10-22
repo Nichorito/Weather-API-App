0d6dde373c2f066704fccaded8154862045f168f
Nichorito <nsheppard999@gmail.com>
2024-10-22 19:08:09 -0400

    Finish the current weather section

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './style.css';
 const currentTemp = document.querySelector('#current-temp');
 const conditions = document.querySelector('#conditions');
 const feelsLike = document.querySelector('#feels-like')
+const highLow = document.querySelector('#high-and-low')
+const location = document.querySelector('#location')
 
 async function getCurrentWeather() {
     const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/montreal?key=XUKBP22J6BJECGNG7BAMGAX9T', {mode: 'cors'});
@@ -11,7 +13,8 @@ async function getCurrentWeather() {
     currentTemp.textContent = convertToCelsius(weatherData.currentConditions.temp) + "°";
     conditions.textContent = weatherData.currentConditions.conditions;
     feelsLike.textContent = "Feels like: " + convertToCelsius(weatherData.currentConditions.feelslike) + "°";
-
+    highLow.textContent = "H: " + convertToCelsius(weatherData.days[0].feelslikemax) + " L: " + convertToCelsius(weatherData.days[0].feelslikemin);
+    location.textContent = weatherData.resolvedAddress;
   }
 
 function convertToCelsius(f) {
