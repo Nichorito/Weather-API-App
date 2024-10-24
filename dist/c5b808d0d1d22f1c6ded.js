2e1980490f0efbdb4644a46aed905dc03d1115c6
Nichorito <nsheppard999@gmail.com>
2024-10-24 09:55:06 -0400

    Add gif Div

--- dist/c5b808d0d1d22f1c6ded.js
+++ dist/c5b808d0d1d22f1c6ded.js
@@ -1,7 +1,6 @@
 import './style.css';
 
 const rightDiv = document.querySelector('#rightDiv') 
-const gifDiv = document.querySelector('#gifContainer');
 
 const currentTemp = document.querySelector('#current-temp');
 const conditions = document.querySelector('#conditions');
@@ -26,15 +25,10 @@ function convertToCelsius(f) {
 }
 
 window.storeInput = function storeInput() { 
-    console.log('Storing input')
     rightDiv.classList.remove('hidden')
     rightDiv.classList.add('visible')
-    gifDiv.classList.remove('hidden')
-    gifDiv.classList.add('visible')
     getCurrentWeather();
 }
-
-console.log('JS IS WORKING')
 
 //FORM SUBMISSION
 const searchForm = document.querySelector('#searchForm');
@@ -42,13 +36,4 @@ const searchForm = document.querySelector('#searchForm');
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the form from reloading the page
     storeInput(); // Call your function to handle the input
-
-    const img = document.querySelector('img');
-    
-      async function getCats() {
-        const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rh5K3XbNZfopbszfFBguJ0LHBtPRGgjA&s=beach day', {mode: 'cors'});
-        const catData = await response.json();
-        img.src = catData.data.images.original.url;
-      }
-      getCats();
 });
