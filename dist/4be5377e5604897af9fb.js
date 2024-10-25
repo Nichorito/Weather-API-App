ed0eaeb202a98a9ba4962bab30b2734e827afc16
Nichorito <nsheppard999@gmail.com>
2024-10-24 21:06:23 -0400

    Add gif functionality back

--- dist/4be5377e5604897af9fb.js
+++ dist/4be5377e5604897af9fb.js
@@ -224,11 +224,11 @@ searchForm.addEventListener('submit', function (event) {
   storeInput(); // Call your function to handle the input
 
   const img = document.querySelector('img');
-/*
+
   async function getCats() {
     const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=Rh5K3XbNZfopbszfFBguJ0LHBtPRGgjA&s=beach day', { mode: 'cors' });
     const catData = await response.json();
     img.src = catData.data.images.original.url;
   }
-  getCats();*/
+  getCats();
 });
